c532b8f5f0edcfaca7b1d28e8fddf23b47e13305
Shiva Malhotra <Shiva.malhotra.email@gmail.com>
2016-01-06 18:32:47 -0800

    Star
    stars

---
+++ src/JB.java
@@ -0,0 +1,45 @@
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+public class JB {
+public static void main(String[] args) {
+	
+
+	
+for (int n = 0; n < 2; n++) {
+	
+
+for (int i = 0; i < 3; i++) {
+		
+	System.out.println("Baby");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	} 
+	if(n == 0){
+	System.out.println("oh");
+	}
+	else if(n == 1){
+		System.out.println("no");
+		
+	}
+	
+	}
+	
+	System.out.println("Thought you'd always be mine!");
+}
+}
+
+
+
