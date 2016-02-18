46c542e9bbbad2ca81a4dde9250544a5d829d913
tuboss4u <league@WTS10.attlocal.net>
2016-02-17 18:30:11 -0800

    fib
    fib

---
+++ src/Fibonachi.java
@@ -0,0 +1,18 @@
+
+public class Fibonachi {
+	public static void main(String[] args) {
+		int x = 0;
+		int y = 1;
+		
+		for (int i = 0; i < 23; i++) {
+			
+			
+			
+		 System.out.println(x);
+		 x = x+y;
+		 System.out.println(y);
+		 y = x+y;
+		
+		}
+	}
+}
